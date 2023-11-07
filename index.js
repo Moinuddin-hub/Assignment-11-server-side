af1558048e6531a778e532e789b7dc5fbed106a7
Moinuddin-hub <moincse022@gmail.com>
2023-11-07 20:10:47 +0600

    single user section done

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require("cors");
+require('dotenv').config();
 const app = express()
-
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const port = 5000
-// s1uZMzeJP4EJlVrh
 
-const uri = "mongodb+srv://moincse022:s1uZMzeJP4EJlVrh@cluster0.b6dpetz.mongodb.net/?retryWrites=true&w=majority";
+// s1uZMzeJP4EJlVrh
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASS)
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b6dpetz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -41,7 +43,13 @@ async function run() {
     const result=await cursor.toArray();
     res.send(result);
   })
-
+  app.get(`/users/:id`,async(req,res)=>{
+    const id=req.params.id;
+    const query={_id: new ObjectId(id)};
+    console.log(query);
+    const result = await userCollection.findOne(query);
+    res.send(result);
+})
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
