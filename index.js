0809350663c2d92e11da4a776d8e7b2bdefea557
Moinuddin-hub <moincse022@gmail.com>
2023-11-09 18:55:57 +0600

    finally done

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+      origin: ['http://localhost:5173', 'https://fir-auth-7b78b.web.app'],
+      credentials: true,
+  }),
+)
 app.use(express.json());
 
 
